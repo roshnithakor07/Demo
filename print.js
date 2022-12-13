0c43c14c062c0268c622b3fe8159c417e48cd28a
roshnithakor07 <thakorroshni@gmail.com>
2022-12-13 15:17:47 +0530

    first commit

---
+++ print.js
@@ -0,0 +1,3 @@
+let pageheader = document.getElementById('page-header');
+
+pageheader.innerHTML = new Date().toDateString() + ` <span style="margin-left: 380px;">Trend Micro Health Check Report</span>`
